f3c0a69c69a526aad75acd06dd4ee1694f1258f0
Bhavya Gor <bhavya.gor9999@gmail.com>
2023-03-19 22:20:31 +0530

    both contracts updated

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -1,45 +1,33 @@
-// SPDX-License-Identifier:MIT
+// I'm a comment!
+// SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.7;
 
+// pragma solidity ^0.8.0;
+// pragma solidity >=0.8.0 <0.9.0;
+
 contract SimpleStorage {
-    //boolean,unit,int,address,bytes
-    uint public favouriteNumber; //DEFAULT IS 0
+    uint256 favoriteNumber;
 
-    // bool hasFavoriteNumber = true;
-    // string name = "Bhavya";
-    // address myAddress = 0xf35239d2c73c1f0e1E5ee8D174E0479a4040c26C;
-
-    //functions
-    function store(uint256 _favoriteNumber) public {
-        favouriteNumber = _favoriteNumber;
-        //0xd9145CCE52D386f254917e481eB44e9943F39138 -- address of contract
-    }
-
-    //view function just reads from what is present in the contract, meaning there is no modification of state as a result no gas is spent
-    //pure function doesnot allow read / modification of state but instead just performs stuff for the contract that is repeatedly performed like simple math fucntions
-    // both the above functions dont spend gas
-
-    //array and struct
     struct People {
-        uint256 favouriteNumber;
+        uint256 favoriteNumber;
         string name;
     }
+    // uint256[] public anArray;
+    People[] public people;
 
-    //map string name mapped to number
-    mapping(string => uint256) public nameToFavNumber;
+    mapping(string => uint256) public nameToFavoriteNumber;
 
-    // uint256[] public favouriteNumberList;
-    People[] public people; //dynamic array []
+    function store(uint256 _favoriteNumber) public virtual {
+        favoriteNumber = _favoriteNumber;
+    }
 
-    //static array [3]
+    function retrieve() public view returns (uint256) {
+        return favoriteNumber;
+    }
 
-    // People public person = People({favouriteNumber:2,name:"bhavya"});
-
-    function addPerson(string memory _name, uint256 _favNumber) public {
-        // People memory newPerson = People({favouriteNumber:_favNumber,name: _name});
-        people.push(People(_favNumber, _name)); //People object and people is array variable name
-        nameToFavNumber[_name] = _favNumber;
-
-        //  people.push(newPerson);
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        people.push(People(_favoriteNumber, _name));
+        nameToFavoriteNumber[_name] = _favoriteNumber;
     }
 }
--- StorageFactory.sol
+++ StorageFactory.sol
@@ -3,9 +3,29 @@ pragma solidity ^0.8.0;
 import "./SimpleStorage.sol";
 
 contract StorageFactory {
-    SimpleStorage public simpleStorage;
+    SimpleStorage[] public simpleStorageArray;
 
     function createSimpleStorageContract() public {
-        simpleStorage = new SimpleStorage();
+        SimpleStorage simpleStorage = new SimpleStorage();
+        simpleStorageArray.push(simpleStorage);
+    }
+
+    function sfStore(
+        uint256 _simpleStorageIndex,
+        uint256 _simpleStorageNumber
+    ) public {
+        //to interact with a smart contract you are going to need 2 things
+        // Address and the ABI (application binary interface) list of all the inputs/outputs to interact
+
+        //  SimpleStorage simpleStorage = simpleStorageArray[_simpleStorageIndex];
+        //  simpleStorage.store(_simpleStorageNumber);
+
+        simpleStorageArray[_simpleStorageIndex].store(_simpleStorageNumber);
+    }
+
+    function sfGet(uint256 _simpleStorageIndex) public view returns (uint256) {
+        // SimpleStorage simpleStorage = simpleStorageArray[_simpleStorageIndex];
+        // return simpleStorage.retrieve();
+        return simpleStorageArray[_simpleStorageIndex].retrieve();
     }
 }
